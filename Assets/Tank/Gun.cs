12c3949188c72e0388d752be0bba04c6f4411c45
AlexTerra21 <baewoo21@gmail.com>
2017-06-01 04:37:28 +0300

    First

---
+++ Assets/Tank/Gun.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour {
+
+	public GameObject ammoProfab;
+	public int ammo = 10;
+
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKeyDown (KeyCode.Space)) 
+		{
+			Instantiate (ammoProfab);
+			ammo--;
+		}	
+	}
+}
